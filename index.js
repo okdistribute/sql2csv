c1dc8fe8e55f4a5cf4c41069ecacd70b3dd89597
Karissa McKelvey <krmckelv@gmail.com>
2015-04-04 14:57:15 -0700

    Add sqlite, and readme

--- index.js
+++ index.js
@@ -2,24 +2,35 @@ var argv = require('minimist')(process.argv.slice(2));
 var spawn = require('child_process').spawn;
 var debug = require('debug')('sql2csv')
 var parseUrl = require('parse-db-url')
+require('String.prototype.endswith')
 
 module.exports = sql2csv
+
+
 var sql2csv = {
-  postgres: function (url) {
-    var db = url
-    var query = "COPY (" + argv.c + ") TO STDOUT WITH CSV HEADER;"
-    debug('executing', query)
-    streamExecute('psql', [db, '-c ' + query])
+  postgres: function (db, query) {
+    if (query.endsWith(';')) {
+      query = query.substring(0, query.length -1)
+    }
+    var fullQuery = "COPY (" + query + ") TO STDOUT WITH CSV HEADER;"
+    var args = [db, '-c ' + fullQuery]
+    streamExecute('psql', args)
   },
-  mysql: function (url) {
+  mysql: function (db, query) {
 
   },
-  sqlite: function (url) {
-
+  sqlite: function (db, query) {
+    if (!query.endsWith(';')) {
+      query = query + ';'
+    }
+    var fullQuery = '\\"' + query + '\\"'
+    var args = [db, '-header', '-csv', query]
+    streamExecute('sqlite3', args)
   }
 }
 
 function streamExecute(command, args) {
+  debug('executing', command, args)
   var cmd = spawn(command, args, {
     stdio: [
       0, // use parents stdin for child
@@ -29,12 +40,13 @@ function streamExecute(command, args) {
 }
 
 /** MAIN **/
+var USAGE = 'sql2csv <datbase url/location> -c <query> --db <postgres,mysql,sqlite>'
+if (argv._.length != 1) return console.error(USAGE)
+if (!argv.db) return console.error(USAGE)
 
-if (argv._.length != 1) return console.error('sql2csv <datbase url> -c <query> ')
-var URL = argv._[0]
-var dbConfig = parseUrl(URL)
+var url = argv._[0]
 
-debug('using ', dbConfig.adapter)
-var f = sql2csv[dbConfig.adapter]
-if (f) return f(URL)
+debug('using ', argv.db)
+var f = sql2csv[argv.db]
+if (f) return f(url, argv.c)
 else return console.error('Connection string unrecognized. Did you type it right? \n  ' + URL)
--- readme.md
+++ readme.md
@@ -1,2 +1,42 @@
 #psql2csv
-[![NPM](https://nodei.co/npm/psql2csv.png)](https://nodei.co/npm/psql2csv/)
+[![NPM](https://nodei.co/npm/sql2csv.png)](https://nodei.co/npm/sql2csv/)
+
+```bash
+$ npm install -g sql2csv
+```
+
+# Usage
+```bash
+$ sql2csv <datbase url/location> -c <query> --db <postgres,mysql,sqlite>
+```
+## sqlite
+
+```bash
+$ sql2csv test.db -c "SELECT * from users;" --db sqlite
+
+ID,name
+3,karissa
+4,dave
+5,ryan
+```
+
+## postgres
+
+```bash
+$ sql2csv -c "SELECT id,amount,debt_type from users"  "postgres://localhost/debtis"  --db postgres
+
+id,amount,kind
+1,80000,student
+2,80000,student
+3,80000,student
+4,80000,
+5,80000,student
+6,80000,student
+7,80234,student
+8,300000,auto
+```
+
+## mysql
+
+*coming soon*
+
